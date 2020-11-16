cb6f3dfe698ca4eec6c43ac37294ae6bd22f061f
chosson <chosson@users.noreply.github.com>
2020-11-16 11:42:40 -0500

    Moved functions to module.

--- characters.py
+++ characters.py
@@ -89,3 +89,25 @@ class Character:
 		damage = ((level_factor * weapon_factor * atk_def_factor) / 50 + 2) * modifier
 		return int(round(damage)), critical
 
+
+def deal_damage(attacker, defender):
+	damage, crit = attacker.compute_damage(defender)
+	defender.hp -= damage
+	print(f"{attacker.name} used {attacker.weapon.name}")
+	if crit:
+		print("  Critical hit!")
+	print(f"  {defender.name} took {damage} dmg")
+
+def run_battle(c1, c2):
+	attacker = c1
+	defender = c2
+	turn = 1
+	print(f"{attacker.name} starts a battle with {defender.name}!")
+	while True:
+		deal_damage(attacker, defender)
+		if defender.hp == 0:
+			print(f"{defender.name } is sleeping with the fishes.")
+			break
+		attacker, defender = defender, attacker
+		turn += 1
+	return turn
--- exercice.py
+++ exercice.py
@@ -9,29 +9,6 @@ from inspect import *
 from characters import *
 
 
-def deal_damage(attacker, defender):
-	damage, crit = attacker.compute_damage(defender)
-	defender.hp -= damage
-	print(f"{attacker.name} used {attacker.weapon.name}")
-	if crit:
-		print("  Critical hit!")
-	print(f"  {defender.name} took {damage} dmg")
-
-def run_battle(c1, c2):
-	attacker = c1
-	defender = c2
-	turn = 1
-	print(f"{attacker.name} starts a battle with {defender.name}!")
-	while True:
-		deal_damage(attacker, defender)
-		if defender.hp == 0:
-			print(f"{defender.name } is sleeping with the fishes.")
-			break
-		attacker, defender = defender, attacker
-		turn += 1
-	return turn
-
-
 def main():
 	c1 = Character("Äpik", 200, 150, 70, 70)
 	c2 = Character("Gämmor", 250, 100, 120, 60)
