cbeecc2fadd87f7c1251a4c80c3d1b5ae9d0b481
chosson <chosson@users.noreply.github.com>
2020-11-16 12:35:45 -0500

    Removed answers.

--- game.py
+++ game.py
@@ -21,19 +21,6 @@ class Weapon:
 
 	UNARMED_POWER = 20
 
-	def __init__(self, name, power, min_level):
-		self.__name = name
-		self.power = power
-		self.min_level = min_level
-
-	@property
-	def name(self):
-		return self.__name
-
-	@classmethod
-	def make_unarmed(cls):
-		return cls("Unarmed", Weapon.UNARMED_POWER, 1)
-
 
 class Character:
 	"""
@@ -46,70 +33,22 @@ class Character:
 	:param level: Le niveau d'expérience du personnage
 	"""
 
-	def __init__(self, name, max_hp, attack, defense, level):
-		self.__name = name
-		self.level = level
-		self.max_hp = max_hp
-		self.attack = attack
-		self.defense = defense
-		self.weapon = None
-		self.hp = self.max_hp
-
-	@property
-	def name(self):
-		"""
-		Le nom du personnage. Ne peut pas être modifié.
-		"""
-		return self.__name
-
-	@property
-	def weapon(self):
-		return self.__weapon
-
-	@weapon.setter
-	def weapon(self, val):
-		if val is None:
-			val = Weapon.make_unarmed()
-		if val.min_level > self.level:
-			raise ValueError(Weapon)
-		self.__weapon = val
-
-	@property
-	def hp(self):
-		return self.__hp
-
-	@hp.setter
-	def hp(self, val):
-		self.__hp = utils.clamp(val, 0, self.max_hp)
-
-	def compute_damage(self, other):
-		level_factor = (2 * self.level) / 5 + 2
-		weapon_factor = self.weapon.power
-		atk_def_factor = self.attack / other.defense
-		critical = random.random() <= 1/16
-		modifier = (2 if critical else 1) * random.uniform(0.85, 1.0)
-		damage = ((level_factor * weapon_factor * atk_def_factor) / 50 + 2) * modifier
-		return int(round(damage)), critical
 
 
 def deal_damage(attacker, defender):
-	damage, crit = attacker.compute_damage(defender)
-	defender.hp -= damage
+	# TODO: Calculer dégâts
 	print(f"{attacker.name} used {attacker.weapon.name}")
 	if crit:
 		print("  Critical hit!")
 	print(f"  {defender.name} took {damage} dmg")
 
 def run_battle(c1, c2):
-	attacker = c1
-	defender = c2
-	turn = 1
+	# TODO: Initialiser attaquant/défendeur, tour, etc.
 	print(f"{attacker.name} starts a battle with {defender.name}!")
 	while True:
-		deal_damage(attacker, defender)
-		if defender.hp == 0:
+		# TODO: Appliquer l'attaque
+		# TODO: Si le défendeur est mort
 			print(f"{defender.name } is sleeping with the fishes.")
 			break
-		attacker, defender = defender, attacker
-		turn += 1
-	return turn
+		# Échanger attaquant/défendeur
+	# TODO: Retourner nombre de tours effectués
